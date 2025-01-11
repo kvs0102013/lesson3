558c6052d38f4f0d9a2e880539e9382360e49734
kvs0102013 <kvs0102013@gmail.com>
2025-01-11 19:50:36 +0500

    task3.3.3

--- lesson3.py
+++ lesson3.py
@@ -16,7 +16,10 @@
 # a, b, c = 8, 5, 3
 # print((a/(b+c)-a)/c)
 
-bus = 5
-metro = 3
-result = (6 + (10 - bus) * 2) / (metro + 1) + 132 / (2 + bus)
+# bus = 5
+# metro = 3
+# result = (6 + ((10 - bus) ** 2)) / (metro + 1) + (132 / (2 + bus))
+# print(result)
+
+result = (-3 + (8 ** 2 -12) * (4 ** (3 ** 2)))/(2 *18)
 print(result)

146a2d5e28f2499ddd277623c0e1e75ba3409e47
kvs0102013 <kvs0102013@gmail.com>
2025-01-11 07:41:58 +0500

    second_task1

--- lesson3.py
+++ lesson3.py
@@ -1,5 +1,12 @@
-print("Третий урок")
-print(6 * 39)
-print(3 ** 5)
-print(-8 / (-4))
-print(10/2+6)
+# pint("Третий урок")
+# print(6 * 39)
+# print(3 ** 5)
+# print(-8 / (-4))
+# print(10/2+6)
+
+a, b, c = 5, 4, 2
+print(a + b + c )
+print(5-4-2)
+print(5 * 4 * 2)
+print(5 / 4 / 2)
+print(5 ** 4 ** 2)

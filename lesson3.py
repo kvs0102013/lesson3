7c5dcc445f0b4ec03200816bf372c325667b7811
kvs0102013 <kvs0102013@gmail.com>
2025-01-11 07:48:56 +0500

    third_task

--- lesson3.py
+++ lesson3.py
@@ -4,9 +4,11 @@
 # print(-8 / (-4))
 # print(10/2+6)
 
-a, b, c = 5, 4, 2
-print(a + b + c )
-print(5-4-2)
-print(5 * 4 * 2)
-print(5 / 4 / 2)
-print(5 ** 4 ** 2)
+# a, b, c = 5, 4, 2
+# print(a + b + c )
+# print(5-4-2)
+# print(5 * 4 * 2)
+# print(5 / 4 / 2)
+# print(5 ** 4 ** 2)
+
+print(65 / 0)
